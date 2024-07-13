7d7b4e0161d4f51c58b55f213a732c0e4dbac0ee
gabrielPistore <devgbpistore@gmail.com>
2024-07-13 01:03:29 -0300

    update plugins

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,8 +1,8 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  opts = { theme = 'tokyonight' },
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = { theme = "tokyonight" },
   config = function()
-    require('lualine').setup()
+    require("lualine").setup()
   end
 }
