e371eb462c6da237cbe60a08bd8b7c2f361a6c85
gabrielPistore <devgbpistore@gmail.com>
2024-07-15 19:42:37 -0300

    update LSP config and dashboard

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -17,11 +17,11 @@ return {
       [[                                                                       ]],
     }
     dashboard.section.buttons.val = {
-      dashboard.button("e", "[e] New file", ":ene<BAR>startinsert<CR>"),
-      dashboard.button("f", "[f] Find file", ":Telescope find_files<CR>"),
-      dashboard.button("g", "[g] Find text", ":Telescope live_grep<CR>"),
-      dashboard.button("r", "[r] Recently used files", ":Telescope oldfiles<CR>"),
-      dashboard.button("q", "[q] Quit", ":qa<CR>"),
+      dashboard.button("e", "New file", ":ene<BAR>startinsert<CR>"),
+      dashboard.button("f", "Find file", ":Telescope find_files<CR>"),
+      dashboard.button("g", "Find text", ":Telescope live_grep<CR>"),
+      dashboard.button("r", "Recently used files", ":Telescope oldfiles<CR>"),
+      dashboard.button("q", "Quit", ":qa<CR>"),
     }
     alpha.setup(dashboard.opts)
   end,
