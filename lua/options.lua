c62e56047df4e8d798c89fed5e0defa7b6c29a8c
gabrielPistore <devgbpistore@gmail.com>
2024-07-16 17:07:26 -0300

    fix eslint and change theme to gruvbox

--- lua/options.lua
+++ lua/options.lua
@@ -13,7 +13,8 @@ vim.opt.shiftwidth = 2
 vim.opt.shiftround = true
 vim.opt.expandtab = true
 
-vim.wo.number = true
+vim.opt.number = true
+vim.opt.relativenumber = true
 
-vim.cmd([[ set noswapfile ]])
-vim.cmd([[ set termguicolors ]])
+vim.cmd("set noswapfile")
+vim.cmd("set termguicolors")
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,7 +1,7 @@
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = { theme = "tokyonight" },
+  opts = { theme = "gruvbox" },
   config = function()
     require("lualine").setup()
   end,
