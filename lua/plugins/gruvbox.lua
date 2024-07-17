e4b2b7b5ad672302ad4ad96875906d9119769732
gabrielPistore <devgbpistore@gmail.com>
2024-07-16 23:42:19 -0300

    remove transparency

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -2,9 +2,7 @@ return {
   "ellisonleao/gruvbox.nvim",
   priority = 1000,
   config = function()
-    require("gruvbox").setup({
-      transparent_mode = true,
-    })
+    require("gruvbox").setup({})
     vim.cmd("colorscheme gruvbox")
   end,
 }
