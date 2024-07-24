100cbb5d297ef3a63c78655180785165b29d32a1
gabrielPistore <devgbpistore@gmail.com>
2024-07-23 21:29:05 -0300

    update new file icon

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -17,7 +17,7 @@ return {
       [[                                                                       ]],
     }
     dashboard.section.buttons.val = {
-      dashboard.button("e", "  New file", ":ene<BAR>startinsert<CR>"),
+      dashboard.button("e", "  New file", ":ene<BAR>startinsert<CR>"),
       dashboard.button("f", "  Find file", ":Telescope find_files<CR>"),
       dashboard.button("g", "  Find text", ":Telescope live_grep<CR>"),
       dashboard.button("r", "  Recently used files", ":Telescope oldfiles<CR>"),
