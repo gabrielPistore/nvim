a9f42a9602906e171b812364f8817b5679b9f80b
gabrielPistore <devgbpistore@gmail.com>
2024-07-24 21:06:19 -0300

    rename colorscheme to tokyonight

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,8 +1,7 @@
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = { theme = "tokyonight" },
-  config = function(_, opts)
-    require("lualine").setup(opts)
+  config = function()
+    require("lualine").setup()
   end,
 }
