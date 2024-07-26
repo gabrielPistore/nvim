9f5af635edf154dfe2e764a7f5bc45b9097d7c94
gabrielPistore <devgbpistore@gmail.com>
2024-07-25 22:07:09 -0300

    add emmet language server

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,7 +18,13 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver", "pyright", "tailwindcss" },
+        ensure_installed = {
+          "lua_ls",
+          "tsserver",
+          "pyright",
+          "tailwindcss",
+          "emmet_language_server",
+        },
       })
     end,
   },
